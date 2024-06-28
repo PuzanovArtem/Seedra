d7a0c1dd416a23c57f52a85d44444685fe5b722b
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-06-28 23:48:40 +0300

    add main-product

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 
 
 import './src/js/components/header.js'
+import './src/js/pages/main/product.js'
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,9 +1,23 @@
+
+
+
 document.getElementById('menu__burger').addEventListener('click', function() {
     const menu = document.getElementById('menu');
-    menu.classList.toggle('show');
-
     const main = document.querySelector('main');
-     main.classList.toggle('main-blur');
+
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+        main.classList.remove('main-blur');
+        setTimeout(() => {
+            menu.style.display = 'none';
+        }, 300); 
+    } else {
+        menu.style.display = 'flex';
+        setTimeout(() => {
+            menu.classList.add('show');
+            main.classList.add('main-blur');
+        }, 10); 
+    }
 });
 
 const itemList = document.querySelectorAll('.menu__item');
@@ -12,7 +26,10 @@ const main = document.querySelector('main');
 
 itemList.forEach(item => {
     item.addEventListener('click', function () {
-        menu.classList.remove("show");
+        menu.classList.remove('show');
         main.classList.remove('main-blur');
-    })
-})
+        setTimeout(() => {
+            menu.style.display = 'none';
+        }, 300); 
+    });
+});
