5a8e9656e0aef46dab12267f9ab0b1893cb04baf
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-07-11 11:11:04 +0300

    add cart page

---
+++ src/pages/cart/cart.js
@@ -0,0 +1,8 @@
+import '../../js/components/header.js';
+import '../../js/components/select.js';
+import '../../js/components/counter.js';
+
+
+    
+
+
--- vite.config.js
+++ vite.config.js
@@ -56,6 +56,8 @@ export default defineConfig({
         resolve(__dirname, "src/html/pages/main/"),
         resolve(__dirname, "src/html/pages/blog/"),
         resolve(__dirname, "src/html/pages/blog-post/"),
+        resolve(__dirname, "src/html/pages/cart/"),
+        resolve(__dirname, "src/html/pages/checkout/"),
       ]
     }),
   ],
@@ -66,6 +68,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/pages/login/login.html"),
         blog: resolve(__dirname, "src/pages/blog/blog.html"),
         blogPost: resolve(__dirname, "src/pages/blog-post/blog-post.html"),
+        cart: resolve(__dirname, "src/pages/cart/cart.html"),
+        checkout: resolve(__dirname, "src/pages/checkout/checkout.html"),
       },
     },
   },
