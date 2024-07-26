fcc84430e722d6fdff6265839634d5ecb436fbc0
PuzanovArtem <puzanov522@gmail.com>
2024-07-26 19:33:37 +0300

    fix ps product card

--- src/js/pages/card/product-card.js
+++ src/js/pages/card/product-card.js
@@ -11,29 +11,4 @@ document.addEventListener("DOMContentLoaded", function() {
           thumbnail.classList.add('active');
       });
   });
-
-  // Обработчик для сброса к исходному изображению
-  const resetThumbnail = document.querySelector('.reset-thumbnail');
-  if (resetThumbnail) {
-      resetThumbnail.addEventListener('click', function() {
-          const originalSrc = currentImage.getAttribute('data-original');
-          currentImage.src = originalSrc;
-          thumbnails.forEach(thumb => thumb.classList.remove('active'));
-          resetThumbnail.classList.add('active');
-      });
-  }
 });
-
-
-
-function increment(id) {
-  const counterElement = document.getElementById(id);
-  let currentValue = parseInt(counterElement.innerText, 10);
-  counterElement.innerText = currentValue + 1;
-}
-
-function decrement(id) {
-  const counterElement = document.getElementById(id);
-  let currentValue = parseInt(counterElement.innerText, 10);
-  counterElement.innerText = currentValue - 1;
-}
