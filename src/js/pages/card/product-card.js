dd06bc4283c3eb985ac793a3a4142642061ef830
PuzanovArtem <puzanov522@gmail.com>
2024-07-30 21:13:52 +0300

    build fix

--- src/js/pages/card/product-card.js
+++ src/js/pages/card/product-card.js
@@ -3,14 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
   const thumbnails = document.querySelectorAll('.content__thumbnails--img');
   const currentImage = document.getElementById('currentImage');
   
-  
   thumbnails.forEach(thumbnail => {
-      thumbnail.addEventListener('click', function() {
-          const src = thumbnail.getAttribute('data-src');
-          currentImage.src = src;
-          thumbnails.forEach(thumb => thumb.classList.remove('active'));
-          thumbnail.classList.add('active');
-      });
+    thumbnail.addEventListener('click', function() {
+      const src = thumbnail.getAttribute('data-src');
+      currentImage.src = src;
+      thumbnails.forEach(thumb => thumb.classList.remove('active'));
+      thumbnail.classList.add('active');
+    });
   });
 });
 
