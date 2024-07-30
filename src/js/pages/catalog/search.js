73e43fffd2e41058479e2f9594d78ae749155acb
PuzanovArtem <puzanov522@gmail.com>
2024-07-30 13:59:16 +0300

    new mob filter

--- src/js/pages/catalog/search.js
+++ src/js/pages/catalog/search.js
@@ -72,33 +72,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// popup-filter
 document.addEventListener('DOMContentLoaded', () => {
-    const accordions = document.querySelectorAll('.filter__popup__accordion');
+    const accordions = document.querySelectorAll('.popup-accordion');
     accordions.forEach(accordion => {
-        accordion.addEventListener('click', () => {
-            const panel = accordion.nextElementSibling;
-            const arrow = accordion.querySelector('.filter__popup__arrow');
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-                arrow.classList.remove('rotate');
-            } else {
-                panel.style.display = "block";
-                arrow.classList.add('rotate');
-            }
-        });
+      accordion.addEventListener('click', () => {
+        const panel = accordion.nextElementSibling;
+        const arrow = accordion.querySelector('.popup-arrow');
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+          arrow.classList.remove('rotate');
+        } else {
+          panel.style.display = "block";
+          arrow.classList.add('rotate');
+        }
+      });
     });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const rangeInput = document.getElementById('styled-range');
-
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const rangeInput = document.getElementById('popup-styled-range');
+  
     function updateRangeBackground() {
-        const value = (rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
-        rangeInput.style.background = `linear-gradient(to right, #4CAF50 ${value}%, #e1e1e1 ${value}%)`;
+      const value = (rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
+      rangeInput.style.background = `linear-gradient(to right, #4CAF50 ${value}%, #e1e1e1 ${value}%)`;
     }
-
+  
     rangeInput.addEventListener('input', updateRangeBackground);
-
-    // Initialize background on load
+  
     updateRangeBackground();
-});
+  });
+  
