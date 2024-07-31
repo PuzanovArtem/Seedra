e244ff40eefe1a65c71b4a2efaa98f90f00e5891
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-07-31 20:05:54 +0300

    clear file

--- src/pages/blog-post/blog-post.js
+++ src/pages/blog-post/blog-post.js
@@ -1,25 +1,3 @@
 import '.././../js/components/header.js';
 
 
-const urlParams = new URLSearchParams(window.location.search);
-    const postId = urlParams.get('id');
-
-    if (postId) {
-        fetchPostDetails(postId);
-    }
-
-
-async function fetchPostDetails(postId) {
-    try {
-        const response = await fetch(`https://666b067e7013419182d1f622.mockapi.io/api/nuts/product/product/${postId}`);
-        const post = await response.json();
-        displayPostDetails(post);
-    } catch (error) {
-        console.log('Error fetching post details:', error);
-    }
-}
-
-function displayPostDetails(post) {
-    
-    document.getElementById('post-title').innerText = post.title;
-}
