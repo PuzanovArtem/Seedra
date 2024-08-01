7d2e682b443bc69b7b2312d79e1813420a51f487
PuzanovArtem <puzanov522@gmail.com>
2024-08-01 11:22:51 +0300

    bug fix scroll

--- docs/assets/main-hABsIwai.js
+++ docs/assets/main-hABsIwai.js
@@ -1,10 +1,13 @@
-import"./header-rZn2fZ3j.js";import"./search-DYLUfN49.js";const l="https://dummyjson.com/products/categories";async function p(){try{const c=await(await fetch(l)).json();d(c)}catch(a){console.error("Error fetching categories:",a)}}function d(a){const c=document.getElementById("catalog-category-list");a.forEach(s=>{const t=document.createElement("li");t.className="catalog__category-item catalog__category-btn",t.innerHTML=`
-            <span class="main-product__icon"></span>   
+import"./header-BIkM6GJ9.js";import"./swiper-CLjSId5F.js";const l="https://dummyjson.com/products/categories";async function p(){try{const c=await(await fetch(l)).json();d(c)}catch(a){console.error("Error fetching categories:",a)}}function d(a){const c=document.getElementById("main-category-list");a.forEach(s=>{const t=document.createElement("li");t.className="main-product__category-item category-btn",t.innerHTML=`
+        
+            <span class="main-product__icon"></span>        
             <p>${s.name}</p>
-        `,c.appendChild(t)})}p();const o="https://dummyjson.com/products?limit=9";console.log(o);async function m(){try{const s=(await(await fetch(o)).json()).products;u(s)}catch(a){console.log(a)}}function u(a){const c=document.getElementById("main-product__list");a.forEach(s=>{const t=document.createElement("li");t.classList="main-product__item",t.innerHTML=`<div class="main-product__heard-wrapper">
+        `,c.appendChild(t)})}p();const o="https://dummyjson.com/products?limit=6";console.log(o);async function m(){try{const s=(await(await fetch(o)).json()).products;u(s)}catch(a){console.log(a)}}function u(a){const c=document.getElementById("main-product__list");a.forEach(s=>{const t=document.createElement("li");t.classList="main-product__item",t.innerHTML=`<a href="/Seedra/src/pages/card/card.html">
+        <div class="main-product__heard-wrapper">
                     <span class="main-product__heard-full"></span>
                     <span class="main-product__heard-fill"></span>
                     
+
                 </div>
 
                 <img src="${s.images[0]}" alt="img" width="294" height="294" loading="lazy">
@@ -36,4 +39,5 @@ import"./header-rZn2fZ3j.js";import"./search-DYLUfN49.js";const l="https://dummy
                 <div class="main-product__price-wrapper">
                     <p class="main-product__price">$${s.price}</p>
                     <button class="main-product__cart-btn cart-btn" aria-label="cart"></button>
-                </div>`,c.appendChild(t);const r=t.querySelector(".main-product__heard-wrapper"),e=t.querySelector(".main-product__heard-full"),i=t.querySelector(".main-product__heard-fill");r.addEventListener("click",function(){r.classList.toggle("active"),r.classList.contains("active")?(e.style.display="none",i.style.display="block"):(e.style.display="block",i.style.display="none")});const n=t.querySelector(".main-product__cart-btn");n.addEventListener("click",function(){n.classList.toggle("active")})})}m();
+                </div>
+                </a>`,c.appendChild(t);const r=t.querySelector(".main-product__heard-wrapper"),e=t.querySelector(".main-product__heard-full"),i=t.querySelector(".main-product__heard-fill");r.addEventListener("click",function(){r.classList.toggle("active"),r.classList.contains("active")?(e.style.display="none",i.style.display="block"):(e.style.display="block",i.style.display="none")});const n=t.querySelector(".main-product__cart-btn");n.addEventListener("click",function(){n.classList.toggle("active")})})}m();
